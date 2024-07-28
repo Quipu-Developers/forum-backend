2f45d5929a5f04f72c3ae281dbb1b25054aab4ea
YenaLey <yena.e121@gmail.com>
2024-07-29 03:39:45 +0900

    Chore: index.js config에서 값 가져오는 방식으로 수정

--- models/index.js
+++ models/index.js
@@ -1,17 +1,12 @@
 const { Sequelize, DataTypes } = require("sequelize");
-const dotenv = require("dotenv");
 
-dotenv.config();
+const config = require(__dirname + "/../config/config.json")["development"];
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    port: process.env.DB_PORT,
-  }
+  config.database,
+  config.username,
+  config.password,
+  config
 );
 
 const User = require("./user")(sequelize, DataTypes);
