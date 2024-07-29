1dfb8cebe20abac91d8d86009cf4e3e449654c97
ejjem <tigonlee0627@gmail.com>
2024-07-30 01:04:33 +0900

    디렉토리 구조 수정

--- src/models/post.js
+++ src/models/post.js
@@ -1,73 +1,82 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  const Post = sequelize.define(
-    "Post",
-    {
-      post_id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      board_type: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      user_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      user_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      post_time: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      edit_time: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      file_name: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      file_size: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-      },
-      file_type: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      file_url: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      content: {
-        type: DataTypes.JSON,
-        allowNull: false,
-      },
-    },
-    {
-      tableName: "Posts",
-      timestamps: true,
-        charset: 'utf8mb4',
-        collate: 'utf8mb4_general_ci',
-    }
-  );
+    const Post = sequelize.define(
+        "Post",
+        {
+            post_id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            board_type: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            user_name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            user_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                    model: "Users",
+                    key: "user_id",
+                },
+                onDelete: "SET NULL",
+            },
+            title: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            post_time: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            edit_time: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+            file_name: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+            file_size: {
+                type: DataTypes.INTEGER,
+                allowNull: true,
+            },
+            file_type: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+            file_url: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+            content: {
+                type: DataTypes.JSON,
+                allowNull: false,
+            },
+        },
+        {
+            tableName: "Posts",
+            timestamps: true,
+            charset: "utf8mb4",
+            collate: "utf8mb4_general_ci",
+        }
+    );
 
-  Post.associate = function (models) {
-    Post.hasMany(models.Comment, {
-      foreignKey: "post_id",
-      sourceKey: "post_id",
-    });
-  };
+    Post.associate = function (models) {
+        Post.belongsTo(models.User, {
+            foreignKey: "user_id",
+            targetKey: "user_id",
+        });
+        Post.hasMany(models.Comment, {
+            foreignKey: "post_id",
+            sourceKey: "post_id",
+        });
+    };
 
-  return Post;
+    return Post;
 };
--- src/models/user.js
+++ src/models/user.js
@@ -1,46 +1,53 @@
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define(
-    "User",
-    {
-      user_id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      user_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      student_id: {
-          type: DataTypes.STRING,
-          uniqueKey: true,
-          allowNull: false,
-      },
-      major: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-    },
-    {
-      tableName: "Users",
-      timestamps: true,
-        paranoid: true,
-        charset: 'utf8',
-        collate: 'utf8_general_ci',
-    }
-  );
+    const User = sequelize.define(
+        "User",
+        {
+            user_id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            user_name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            student_id: {
+                type: DataTypes.STRING,
+                uniqueKey: true,
+                allowNull: false,
+            },
+            major: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+        },
+        {
+            tableName: "Users",
+            timestamps: true,
+            paranoid: true,
+            charset: "utf8",
+            collate: "utf8_general_ci",
+        }
+    );
 
-  User.associate = function (models) {
-    // Define associations here
-  };
+    User.associate = function (models) {
+        User.hasMany(models.Post, {
+            foreignKey: "user_id",
+            sourceKey: "user_id",
+        });
+        User.hasMany(models.Comment, {
+            foreignKey: "user_id",
+            sourceKey: "user_id",
+        });
+    };
 
-  return User;
+    return User;
 };
