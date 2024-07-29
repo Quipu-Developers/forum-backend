b78d21f87ca6ff4e81e1d3d4ccc027c0a44a3257
ejjem <tigonlee0627@gmail.com>
2024-07-30 01:01:27 +0900

    디렉토리 구조 수정

---
+++ src/models/user.js
@@ -0,0 +1,46 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      user_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      user_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      student_id: {
+          type: DataTypes.STRING,
+          uniqueKey: true,
+          allowNull: false,
+      },
+      major: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "Users",
+      timestamps: true,
+        paranoid: true,
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+    }
+  );
+
+  User.associate = function (models) {
+    // Define associations here
+  };
+
+  return User;
+};
