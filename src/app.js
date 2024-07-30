dcaf95435cb8f5bbd559fd17f92dc7a6bf23dbba
ejjem <tigonlee0627@gmail.com>
2024-07-31 02:19:35 +0900

    models 수정, app.js 수정

--- src/app.js
+++ src/app.js
@@ -65,3 +65,29 @@ async function DBConnections() {
 }
 
 DBConnections();
+
+
+async function DBConnections() {
+    try {
+        // forum 스키마에 대한 인증 및 동기화
+        await forumSequelize.authenticate();
+        console.log("forum 스키마 DB 연결");
+        await forumSequelize.sync({});
+        console.log("forum 스키마 DB 동기화");
+
+        // joinquipu 스키마에 대한 인증 및 동기화
+        await joinquipuSequelize.authenticate();
+        console.log("joinquipu 스키마 DB 연결");
+        await joinquipuSequelize.sync({});
+        console.log("joinquipu 스키마 DB 동기화");
+
+        // 서버 시작
+        app.listen(PORT, () => {
+            console.log(`서버가 포트 ${PORT}에서 실행 중입니다.`);
+        });
+    } catch (err) {
+        console.error("DB 연결 실패:", err);
+    }
+}
+
+DBConnections();
